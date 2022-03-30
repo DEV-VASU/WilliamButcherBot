8454eeb8a89c7d4f8f320df7da6b0ec673ed7fb0
DEV-VASU <101116280+DEV-VASU@users.noreply.github.com>
2022-03-30 17:37:17 +0530

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -117,8 +117,8 @@ home_keyboard_pm = InlineKeyboardMarkup(
                 text="Commands ❓", callback_data="bot_commands"
             ),
             InlineKeyboardButton(
-                text="Repo 🛠",
-                url="https://github.com/thehamkercat/WilliamButcherBot",
+                text="👤 Bot Owner",
+                user_id = 1926090919,
             ),
         ],
         [
@@ -127,12 +127,12 @@ home_keyboard_pm = InlineKeyboardMarkup(
                 callback_data="stats_callback",
             ),
             InlineKeyboardButton(
-                text="Support 👨", url="http://t.me/WBBSupport"
+                text="🔎 Inline", switch_inline_query_current_chat = ''
             ),
         ],
         [
             InlineKeyboardButton(
-                text="Add Me To Your Group 🎉",
+                text="✚ Add Me To Your Group",
                 url=f"http://t.me/{BOT_USERNAME}?startgroup=new",
             )
         ],
@@ -153,8 +153,8 @@ keyboard = InlineKeyboardMarkup(
                 url=f"t.me/{BOT_USERNAME}?start=help",
             ),
             InlineKeyboardButton(
-                text="Repo 🛠",
-                url="https://github.com/thehamkercat/WilliamButcherBot",
+                text="👤 Bot Owner",
+                user_id = 1926090919,
             ),
         ],
         [
@@ -162,7 +162,7 @@ keyboard = InlineKeyboardMarkup(
                 text="System Stats 💻",
                 callback_data="stats_callback",
             ),
-            InlineKeyboardButton(text="Support 👨", url="t.me/WBBSupport"),
+            InlineKeyboardButton(text="🔎 Inline", switch_inline_query_current_chat = '' ),
         ],
     ]
 )
